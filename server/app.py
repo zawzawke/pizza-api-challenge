01b04a91f3294a61ac4eb2363035b1739af944a8
Paul <paulallanmain@gmail.com>
2025-06-18 11:27:27 +0300

    Initialize Flask project with MVC structure, environment setup, and migration configuration

---
+++ server/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object("server.config.Config")
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    
+    return app
